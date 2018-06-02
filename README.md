576ca320e75c45609a9c471ac8974db984bf4cf8
Eccenux <eccenux+git@gmail.com>
2018-06-02 19:27:59 +0200

    meta and README

---
+++ README.md
@@ -0,0 +1,11 @@
+# IITC plugin: Short portal info
+
+This plugin shows small box with a basic portal information. This is similar to mobile IITC, but also shows current owener and mods installed.
+
+See also: [my other IITC plugins](https://github.com/search?q=user%3AEccenux+iitc-plugin&type=Repositories).
+
+Installation
+------------
+
+Assuming you already have IITC just &rarr; **[install the script](https://github.com/Eccenux/iitc-plugin-short-portal-info/raw/master/short-portal-info.user.js)**.
+
---
+++ short-portal-info.meta.js
@@ -0,0 +1,13 @@
+﻿// ==UserScript==
+// @id             iitc-plugin-short-portal-info@eccenux
+// @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
+// @name           IITC plugin: Short portal info
+// @category       Misc
+// @version        0.2.2
+// @description    [0.2.2] Shows small box with a basic portal information. This is similar to mobile info.
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @grant          none
+// ==/UserScript==
